5ece00bf00707fc452d759593e17ba4dc6f86963
Sean D'Souza <sdammobubbles@gmail.com>
2020-05-23 13:41:46 -0400

    Add error handling for rate limiting and query errors

--- kube/update-image.sh
+++ kube/update-image.sh
@@ -2,24 +2,30 @@
 QUERY=`curl https://api.github.com/repos/seendsouza/acn-bot/actions/runs 2> /dev/null`
 # status completed and conclusion success
 COMMIT=`echo $QUERY | jq -r '[.workflow_runs[] | select(.status == "completed" and .conclusion == "success").head_sha][0]'`
-TAG="sha-${COMMIT:0:7}"
-echo "Tag: $TAG"
 
-DIR="$(cd "$(dirname "$0")" && pwd)"
-FILE="$DIR/ACN_COMMIT"
-if [ ! -f $FILE ]
+if [ ${#COMMIT} == 40 ]
 then
-    echo "default" > $FILE
-fi
-ACN_COMMIT=$(cat "$FILE")
-
-if [ "$COMMIT" != "$ACN_COMMIT" ]
-then
-    echo "Updating acn-bot image."
-    echo $COMMIT > $FILE
-
-    KUBE_FILE=$DIR/deploy.yaml
-
-    sed -i "s/\\(seendsouza\/acn-bot:\\).*/\\1$TAG/" "$KUBE_FILE"
-    kubectl apply -f $KUBE_FILE
+    TAG="sha-${COMMIT:0:7}"
+    echo "Tag: $TAG"
+    
+    DIR="$(cd "$(dirname "$0")" && pwd)"
+    FILE="$DIR/ACN_COMMIT"
+    if [ ! -f $FILE ]
+    then
+        echo "default" > $FILE
+    fi
+    ACN_COMMIT=$(cat "$FILE")
+    
+    if [ "$COMMIT" != "$ACN_COMMIT" ]
+    then
+        echo "Updating acn-bot image."
+        echo $COMMIT > $FILE
+    
+        KUBE_FILE=$DIR/deploy.yaml
+    
+        sed -i "s/\\(seendsouza\/acn-bot:\\).*/\\1$TAG/" "$KUBE_FILE"
+        kubectl apply -f $KUBE_FILE
+    fi
+else
+    echo "Query error"
 fi
